168595162846ba2688ab95c81924aca502bf262e
Jayden <jmneale@waimea.school.nz>
2025-03-25 15:07:53 +1300

    pt2

--- src/Main.kt
+++ src/Main.kt
@@ -11,7 +11,7 @@
  * =====================================================================
  */
 
-const val EMPTY = " ---"     // Represents an empty square
+const val EMPTY = " "     // Represents an empty square
 const val NUMSQUARE = 20 // Number of squares
 
 fun main() {
@@ -21,15 +21,17 @@ fun main() {
     println("-------------------------------------------------------------")
     println("Welcome to the Old Gold Game!")
     println("-------------------------------------------------------------")
+    println("Old Gold is a two-player game, played on a one-dimensional grid with coins,")
+    println("where the aim is to win by being the player who removes the gold coin.")
+    println("-------------------------------------------------------------")
     println()
 
-
-    println("Hello there player 1, please enter your name!")
+    println("Player 1, please enter your name!")
     val player1 = readln()
     println("$player1, you are player 1")
     println()
 
-    println("Hello there player 2, please enter your name!")
+    println("Player 2, please enter your name!")
     val player2 = readln()
     println("$player2, you are player 2")
     println()
@@ -38,11 +40,6 @@ fun main() {
     val coin = mutableListOf<String>()
 
     //-------------------------------------------------
-    printSquare(square)
-    println()
-
-    showCoins(coin)
-    println(monkeyCounter(coin))
 
     coin.add("Coin")
     coin.add("Coin")
@@ -51,7 +48,17 @@ fun main() {
     coin.add("Coin")
     coin.add("Gold")
 
+    showCoins(coin)
+    println((coin))
+
+    printGameBox(square)
+    println()
+
+    placeCoinInSquare(cageSquare)
+    println()
 }
+
+
 
 
 fun setupBoard(): MutableList<String> {
@@ -61,12 +68,14 @@ fun setupBoard(): MutableList<String> {
 }
 
 fun showCoins(monkeyList: List<String>) {
-    println("Monkey List")
-    println("-------------------")
+    println("List of the coins | 5 Normal and 1 gold")
+    println("------------------------------------")
+}
 
-    fun placeCoinInSquare(cageList: MutableList<String>, cageSquare: Int, coin: String) {
+    fun placeCoinInSquare(cageList: MutableList<String>, coin: String) {
         println("Putting $coin into cage $cageSquare")
         cageList[cageSquare - 1] = coin
+
     }
 
     fun listAllMonkeysAndCages(cageList: List<String>) {
@@ -81,18 +90,21 @@ fun showCoins(monkeyList: List<String>) {
     }
 
 
-    fun printSquare(cageList: List<String>) {
+    fun printGameBox(cageList: List<String>) {
 
-        println("+--------+--------+--------+--------+--------+--------+--------+--------+")
+        println("+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+")
         for (i in 0..cageList.size - 1) {
-            print("| ${i + 1} ".padEnd(9))
+            print("| ${i + 1} ".padEnd(4))
         }
         println("|")
-        println("+--------+--------+--------+--------+--------+--------+--------+--------+")
+        println("+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+")
         for (i in 0..cageList.size - 1) {
-            print("| ${cageList[i]} ".padEnd(9))
+            print("| ${cageList[i]} ".padEnd(4))
         }
+        println("|")
+        println("+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+")
     }
-}
+
+
 
 
